9752c84780e61850103f8306511ce4ae391da17d
niyeronsin <niyeronsin2@gmail.com>
2016-01-20 20:16:28 -0500

    Update PlotHW1.py

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -18,3 +18,28 @@
 #Name:
 #Date:
 
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import sin, cos, pi
+
+x = np.linspace(0,6*pi,1000)
+
+y_sin = sin(x)
+
+y_cos = cos(x)
+
+line1, = plt.plot(x, y_sin, '--', linewidth=2)
+dashes = [10, 5, 20, 5]  # 10 points on, 5 off, 100 on, 5 off
+line1.set_dashes(dashes)
+
+line2, = plt.plot(x, y_cos, '-', linewidth=2)
+
+plt.legend(('sin(x)','cos(x)'))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Homework 1')
+plt.show()
+
+#Name: Olaniyi Nafiu
+#ID: 02765881
+#Date: 1/19/2016
